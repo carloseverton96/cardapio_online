97558f300cf8a07bf2752affb0fc2da5821524db
carloseverton96 <140563844+carloseverton96@users.noreply.github.com>
2023-12-20 21:28:12 -0300

    20-12-23 1.3

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ var cardapio = {};
 
 var MEU_CARRINHO = [];
 
+var VALOR_CARRINHO = 0;
+
+var VALOR_ENTREGA  = 5;
+
 cardapio.eventos = {
 
     init: () =>{
@@ -171,7 +175,7 @@ cardapio.metodos = {
 
         if (etapa == 2) {
             $("#lblTituloEtapa").text('Endereço de entrega:');
-            $("#itens Carrinho").addClass('hidden');
+            $("#itensCarrinho").addClass('hidden');
             $("#localEntrega").removeClass('hidden');
             $("#resumoCarrinho").addClass('hidden');
 
@@ -228,10 +232,16 @@ cardapio.metodos = {
                 .replace(/\${qntd}/g, e.qntd)
 
                 $("#itensCarrinho").append(temp);
+
+                //último item
+                if ((i +1) == MEU_CARRINHO.length) {
+                    cardapio.metodos.carregarValores();
+                }
             })
 
-       }else{
-        $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i> Seu carrinho está vazio.</p>')
+       } else {
+        $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i> Seu carrinho está vazio.</p>');
+        cardapio.metodos.carregarValores();
        }
 
 
@@ -276,15 +286,43 @@ cardapio.metodos = {
 
         //atualiza o botão carrinho com a quantidade atualizada
         cardapio.metodos.atualizarBadgeTotal();
+
+        //atualiza os valores (R$) totais do carrinho
+        cardapio.metodos.carregarValores();
     },
 
-    //
+    //carrega os valores de subtotal de entrega e total
     carregarValores: () => {
 
+        VALOR_CARRINHO = 0;
 
-        }
+        $("#lblSubTotal").text('R$ 0,00');
+        $("#lblValorEntrega").text('+ R$ 0,00');
+        $("#lblValorTotal").text('R$ 0,00');
 
+        $.each(MEU_CARRINHO, (i, e) => { 
+        
 
+            VALOR_CARRINHO += parseFloat(e.price * e.qntd)
+
+            if ((i + 1) == MEU_CARRINHO.length) {
+                $("#lblSubTotal").text(`R$ ${VALOR_CARRINHO.toFixed(2).replace('.',',')}`);
+                $("#lblValorEntrega").text(`+ R$ ${VALOR_ENTREGA.toFixed(2).replace('.',',')}`);
+                $("#lblValorTotal").text(`R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.',',')}`);
+            }
+        })
+
+        },
+
+        //carregar a etapa endereço
+        carregarEndereco: () => {
+
+            if (MEU_CARRINHO.length <= 0) {
+                cardapio.metodos.mensagem('Seu carrinho está vazio.')
+                return;
+            }
+            cardapio.metodos.carregarEtapa(2);
+        },
 
 
     //mensagens
